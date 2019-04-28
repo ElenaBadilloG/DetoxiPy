cf6ee44081a36c2d4919f78d0c029649008b3461
evsv <vishwanathevs@outlook.com>
2019-04-28 13:26:35 -0500

    Partially closes #7

---
+++ src/utils/global_vals.py
@@ -0,0 +1,10 @@
+"""
+This module contains global flags and dictionaries for use throughout the 
+entire project. 
+"""
+
+SPL_SEQ_DICT = {"emojis": [":)", ":-)", ":(", ":-(", ":-/", ":/", "-_-", ":|", 
+                            ":-|"],
+                "proper nouns": ["republican", "democrat", "trump", "clinton", "hillary"],
+                "punctuations": ["!", "?", "*"]}
+
