b518c7af665bf00140ffb82fca788817a7027a02
evsv <vishwanathevs@outlook.com>
2019-05-21 14:47:29 -0500

    Closes #11; Adds ABCs; adds test cases for freq vectorizer

--- src/featurecreation/vectorizer.py
+++ src/featurecreation/vectorizer.py
@@ -1,45 +1,55 @@
 import pandas as pd
 import numpy as np
 import torch.utils.data as tud
-from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
+from abc import ABC, abstractmethod
 
-class TfidfData(tud.Dataset):
+class BaseVectorizer(ABC):
 
-    def __init__(self, dataset, ngram_range, vocab_size):
+    @abstractmethod
+    def _vectorize(self, dataset):
+        pass
+    
+class FreqVectorizer(tud.Dataset, BaseVectorizer):
+
+    def __init__(self, dataset, ngram_range, vocab_size, vect_type):
         """
-        Class to extract a TF-IDF sparse-matrix given a an iterable (list, 
+        Class to extract a Bag of Words sparse-matrix given a an iterable (list, 
         series, etc) of texts. Implements default tokenizers under the hood. 
         
         :param dataset: Iterable containing the text dataset to construct the
-                        TF-IDF
+                        BoW
         :type dataset: iterable (list, pandas series, numpy series)
         :param ngram_range: tuple with the upper and lower range of ngrams 
-                            for which TF-IDF numbers should be constructed.
+                            for which BoW numbers should be constructed.
                             Ex: (1, 2) means TF-IDF will be constructed for 
                             unigrams and bigrams
         :type ngram_range: tuple
-        :param vocab_size: Top n most common words to be included for the TF-IDF 
+        :param vocab_size: Top n most common words to be included for the BOW 
                            matrix
         :type vocab_size: int
         """
-        self.vectorizer = TfidfVectorizer(ngram_range = ngram_range, 
-                                          max_features = vocab_size)
-        self.tf_idf = self._vectorize(dataset)
+        if vect_type.lower() == "tf-idf":    
+            self.vectorizer = TfidfVectorizer(ngram_range = ngram_range, 
+                                                max_features = vocab_size)
+        elif vect_type.lower() == "bow":
+            self.vectorizer = CountVectorizer(ngram_range = ngram_range, 
+                                                max_features = vocab_size)
+        self.freq_vect = self._vectorize(dataset)
 
     def _vectorize(self, dataset):
         """
         Private method to vectorize the input data
         
         :param dataset: Iterable containing the text dataset to construct the
-                        TF-IDF
+                        BoW
         :type dataset: iterable (list, pandas series, numpy series)
-        :return: Sparse RF-IDF matrix
+        :return: Sparse BoW matrix
         :rtype: sparse np matrix
         """
-
-        tfidf_matrix = self.vectorizer.fit_transform(dataset)
-        return tfidf_matrix
+        freq_term_matrix = self.vectorizer.fit_transform(dataset)
+        return freq_term_matrix
 
     def __getitem__(self, ix):
 
-        return self.tf_idf[ix, :]
+        return self.freq_vect[ix, :]
--- src/featurecreation/vectorizer_test_cases.py
+++ src/featurecreation/vectorizer_test_cases.py
@@ -1,6 +1,6 @@
-# TEST CASES FOR TF-IDF CREATOR - run from root dir
-
 import src.featurecreation.vectorizer as vc
+
+# TEST CASES FOR TF-IDF CREATOR - run from root dir
 
 # Case 1: vanilla run with list input
 testDoc = ["Hello World",
@@ -8,27 +8,60 @@ testDoc = ["Hello World",
            "Hello World! Why won't you talk back to me?",
            "Hello World! Why can't I say anything else apart from Hello World?!?!"]
 
-tstVct = vc.TfidfData(testDoc, (1, 1), 5000)
-print(tstVct.tf_idf.shape)
+tstVct = vc.FreqVectorizer(testDoc, (1, 1), 5000, vect_type = "tf-idf")
+print(tstVct.freq_vect.shape)
 for i, val in enumerate(tstVct.vectorizer.get_feature_names()):
     print("{}: {}".format(i, val))
 # print(tstVct.tf_idf)
 
 # Case 2: bigram run with list input
 
-tstVct = vc.TfidfData(testDoc, (1, 2), 5000)
-print(tstVct.tf_idf.shape)
+tstVct = vc.FreqVectorizer(testDoc, (1, 2), 5000, vect_type = "tf-idf")
+print(tstVct.freq_vect.shape)
 for i, val in enumerate(tstVct.vectorizer.get_feature_names()):
     print("{}: {}".format(i, val))
 # print(tstVct.tf_idf)
 
 # Case 3: Limited Vocab run with list input
-tstVct = vc.TfidfData(testDoc, (1, 2), 10)
+tstVct = vc.FreqVectorizer(testDoc, (1, 2), 10, vect_type = "tf-idf")
 for i, val in enumerate(tstVct.vectorizer.get_feature_names()):
     print("{}: {}".format(i, val))
-print(tstVct.tf_idf.shape)
+print(tstVct.freq_vect.shape)
 # print(tstVct.tf_idf)
 
 # Case 4: Checking inheritance from torch data utils item
-tstVct = vc.TfidfData(testDoc, (1, 1), 5000)
-print(tstVct[1])
+tstVct = vc.FreqVectorizer(testDoc, (1, 1), 5000, vect_type = "tf-idf")
+print(tstVct[2])
+
+# TEST CASES FOR BOW CREATOR - run from root dir
+
+# Case 1: vanilla run with list input
+testDoc = ["Hello World",
+           "Hello World! How are you today!",
+           "Hello World! Why won't you talk back to me?",
+           "Hello World! Why can't I say anything else apart from Hello World?!?!"]
+
+tstVct = vc.FreqVectorizer(testDoc, (1, 1), 5000, vect_type = "bow")
+print(tstVct.freq_vect.shape)
+for i, val in enumerate(tstVct.vectorizer.get_feature_names()):
+    print("{}: {}".format(i, val))
+# print(tstVct.tf_idf)
+
+# Case 2: bigram run with list input
+
+tstVct = vc.FreqVectorizer(testDoc, (1, 2), 5000, vect_type = "bow")
+print(tstVct.freq_vect.shape)
+for i, val in enumerate(tstVct.vectorizer.get_feature_names()):
+    print("{}: {}".format(i, val))
+# print(tstVct.tf_idf)
+
+# Case 3: Limited Vocab run with list input
+tstVct = vc.FreqVectorizer(testDoc, (1, 2), 10, vect_type = "bow")
+for i, val in enumerate(tstVct.vectorizer.get_feature_names()):
+    print("{}: {}".format(i, val))
+print(tstVct.freq_vect.shape)
+# print(tstVct.tf_idf)
+
+# Case 4: Checking inheritance from torch data utils item
+tstVct = vc.FreqVectorizer(testDoc, (1, 1), 5000, vect_type = "bow")
+print(tstVct[2])
