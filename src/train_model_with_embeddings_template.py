efbbacad7cf55d438eeef00b6479276c03a507e3
evsv <vishwanathevs@outlook.com>
2019-06-02 21:25:56 -0500

    Updating the embedder template and files

--- src/train_model_with_embeddings_template.py
+++ src/train_model_with_embeddings_template.py
@@ -1,13 +1,14 @@
 import pandas as pd
 from gensim.models import KeyedVectors
-from dataprep.text_cleaning import TextPrep
+from dataprep.data_prep import TextPrep
 
 # READING THE DATASET
 data_path = "train.csv"
 comment_column = "comment_text"
 
-raw_data = pd.read_csv(data_path, nrows = 150)
+raw_data = pd.read_csv(data_path, nrows = 2000) # Temp # for speed
 raw_data = raw_data[comment_column]
+raw_data = raw_data[0:150] # Temp # for speed
 
 # CLEANING THE INPUT TEXT
 text_prepper = TextPrep()
@@ -18,8 +19,6 @@ clean_data = raw_data.apply(text_prepper.clean, rmCaps = True,
 
 # VECTORISING THE INPUT TEXT
 clean_data = clean_data.apply(text_prepper.tokenize)
-
 loaded_kv = KeyedVectors.load("featurecreation/embeddings/small_sample_vector_100K_ft.kv", mmap = "r")
 vectoriser = lambda lst :  [loaded_kv[wrd] for wrd in lst]
 clean_data = clean_data.apply(vectoriser)
-
