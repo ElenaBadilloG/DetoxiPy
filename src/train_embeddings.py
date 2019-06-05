a0ebe6bba8b8e576657fb4640289e78ea743a390
evsv <vishwanathevs@outlook.com>
2019-06-04 20:07:43 -0500

    Rationalised embedder imports

--- src/train_embeddings.py
+++ src/train_embeddings.py
@@ -1,8 +1,7 @@
 import pandas as pd
 from dataprep.data_prep import TextPrep 
 from featurecreation.feat_create_utils import VocabularyHelper
-from featurecreation.word_embedder import CbowEmbedder
-from gensim.models import FastText
+from gensim.models import FastText, word2vec
 import multiprocessing
 from time import time
 
@@ -27,16 +26,17 @@ embedder = FastText(min_count = 20, window = 2, size = 64, sample = 6e-5,
 
 # Tokenising sentences and building vocab
 clean_data = clean_data.apply(text_prepper.tokenize)
-embedder.build_vocab(clean_data, progress_per = 10000)
 t = time()
+embedder.build_vocab(clean_data, progress_per = 10000)
 print('Time to build vocab: {} mins'.format(round((time() - t) / 60, 2)))
 
 # Training the model
 t = time()
 sample_data = clean_data.sample(100000)
-embedder.train(sample_data, total_examples = 100000, epochs = 30,
+embedder.train(sample_data, total_examples = 100000, epochs = 20,
                report_delay = 1)
 print('Time to train the model: {} mins'.format(round((time() - t) / 60, 2)))
 
 # Exporting the model weights
+
 embedder.wv.save("featurecreation/embeddings/small_sample_vector_100K_ft.kv")
